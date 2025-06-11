4aa5582a9ef506032de104407507cdd57ebeb2e3
Arch_lucas <lucas27ro@gmail.com>
2025-06-11 20:26:00 -0300

    feat: config login page

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,12 +10,12 @@ import desafioRoutes from './routes/desafio.routes.js';
 // console.log("Chave OpenAI:", process.env.OPENAI_API_KEY);  // Adicione isso para verificar se a chave está sendo carregada
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
+
 app.use('/api', desafioRoutes);
 
 app.listen(PORT, () => {
